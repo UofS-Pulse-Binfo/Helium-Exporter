6d1398244ea4fa476f2f1d146a990033216cbbd0
Rey Tan <reytan@reys-imac.gateway.sasktel.net>
2022-01-13 14:40:35 -0600

    Setup data download form

--- theme/helium_exporter_page.tpl.php
+++ theme/helium_exporter_page.tpl.php
@@ -6,50 +6,62 @@
 ?>
 
 <div id="helium-exporter-contariner">
-  <p>
-    Helium is a generic platform in which various data types can be shown in a pedigree context.&nbsp;&nbsp;<a id="helium-exporter-show" href="#">Show me how</a>
-    <a href="<?php print $helium_resource['download']; ?>" target="_blank">Download Helium</a> | <a href="<?php print $helium_resource['information']; ?>" target="_blank">More Information</a>
-  </p>
-
-  <div id="helium-exporter-show-window">
-    <img id="helium-exporter-diagram" src="<?php print $path_module . '/theme/images/application-install-steps.gif'; ?>" alt="How to visualize data using helium" title="How to visualize data using helium" />
-    <div id="helium-exporter-show-steps">
-      <div class="helium-exporter-show-steps-item">
-        <div>
-          <strong>Step #1:</strong>
-          <p>
-            Download and install Helium application in your computer.
-            <a href="<?php print $helium_resource['download']; ?>">&#xbb; How to install Helium</a>
-          </p>
-        </div>
-      </div>
-      <div class="helium-exporter-show-steps-item">
-        <div>
-          <strong>Step #2:</strong>
-          <p>
-            Use the built-in data exporter below to generate a data file.
-            <a href="<?php print $helium_resource['download']; ?>">&#xbb; How to download data</a>
-          </p>
-        </div>
-      </div>
-      <div class="helium-exporter-show-steps-item">
-        <div>  
-          <strong>Step #3:</strong>
-          <p>
-            Load the data file generated in #2 into Helium to visualize.
-           <a href="<?php print $helium_resource['download']; ?>">&#xbb; How to load data into Helium</a>
-          </p>
-        </div>
-      </div>
-      <div class="helium-exporter-clear-float"></div>
+  <div id="helium-exporter-header">  
+    <div id="helium-exporter-title">Data Exporter for Helium</div>
+    <div id="helium-exporter-help">
+      <a href="https://knowpulse.usask.ca/help" target="_blank" alt="help" title="help"><img src="<?php print $path_module . '/theme/images/help.gif'; ?>" style="border: none;" height="25" width="25"></a>
     </div>
-    <div class="helium-exporter-float-right"><a id="helium-exporter-hide" href="#">Okay, Got It!</a></div>
     <div class="helium-exporter-clear-float"></div>
   </div>
+  <div id="helium-exporter-copy" class="helium-exporter-copy-page">
+    <div class="helium-exporter-content-wrapper">
+      <h2>Visualize plant pedigrees and overlay categorical data using Helium:</h2>
+      <p>
+        Helium is a generic platform in which various data types can be shown in a pedigree context.&nbsp;&nbsp;<a id="helium-exporter-show" href="#">Show me how</a><br />
+        <a href="<?php print $helium_resource['download']; ?>" target="_blank">Download Helium</a> | <a href="<?php print $helium_resource['information']; ?>" target="_blank">More Information</a>
+      </p>
+      
+      <div id="helium-exporter-show-window">
+        <img id="helium-exporter-diagram" src="<?php print $path_module . '/theme/images/application-install-steps.gif'; ?>" width="900" height="200" style="border: none" alt="How to visualize data using helium" title="How to visualize data using helium" />
+        <div id="helium-exporter-show-steps">
+         
+          <div class="helium-exporter-show-steps-item">
+            <div>
+              <strong>Step #1:</strong>
+              <p>
+                Download and install Helium viewer<br />application in your computer.
+                <a href="<?php print $helium_resource['download']; ?>">&#xbb; Download and install Helium</a>
+              </p>
+            </div>
+          </div>
 
-  <div id="helium-exporter-download-form">
-    <?php print drupal_render($form['download_data']); ?>
+          <div class="helium-exporter-show-steps-item">
+            <div>
+              <strong>Step #2:</strong>
+              <p>
+                Use the built-in data exporter below</br />to generate a data file.
+                <a href="<?php print $helium_resource['download']; ?>">&#xbb; How to download data</a>
+              </p>
+            </div>
+          </div>
+          <div class="helium-exporter-show-steps-item">
+            <div>
+              <strong>Step #3:</strong>
+              <p>
+                Load the data file generated in step #2<br />into Helium to visualize.
+                <a href="<?php print $helium_resource['download']; ?>">&#xbb; How to load data into Helium</a>
+              </p>
+            </div>
+          </div>
+          <div class="helium-exporter-clear-float"></div>        
+        </div>
+      </div>
+      <div>
+        <?php print drupal_render($form['download_data']); ?>
+      </div>
+    </div>
   </div>
+
 </div>
 
 
--- theme/script/script-helium-exporter-page.js
+++ theme/script/script-helium-exporter-page.js
@@ -4,23 +4,21 @@
  (function ($) {
   Drupal.behaviors.heliumExporter = {
     attach: function (context, settings) {           
-      var w = $('#helium-exporter-contariner').width();
-      
-      $('#helium-exporter-diagram')
-        .css('width', w + 'px')
-        .css('height', w/4.5 + 'px');
-
       var slideWindow = $('#helium-exporter-show-window');
       // Listen to request to show steps.
       $('#helium-exporter-show').click(function(e) {
         e.preventDefault();
-        slideWindow.slideDown(200);
-      });
+        var t = $(this).text();
 
-      // Listen to when user gets all steps.
-      $('#helium-exporter-hide').click(function(e) {
-        e.preventDefault();
-        slideWindow.slideUp(200);
+        if (t == 'Show me how') {
+          $('#helium-exporter-diagram').fadeIn('slow');
+          $(this).text('Okay, Got It!');
+          slideWindow.slideDown(200);
+        }
+        else {
+          $('#helium-exporter-diagram').fadeOut('fast');
+          $(this).text('Show me how');
+          slideWindow.slideUp(200);
+        }
       });
-      
 }};}(jQuery));
