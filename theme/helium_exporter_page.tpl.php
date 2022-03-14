ecf61b909f501eab8ff8e0add4771ed73520a08c
reynoldltan@gmail.com <reynoldltan@gmail.com>
2022-03-14 11:03:49 -0600

    Makes help url configurable

--- theme/helium_exporter_page.tpl.php
+++ theme/helium_exporter_page.tpl.php
@@ -9,7 +9,10 @@
   <div id="helium-exporter-header">  
     <div id="helium-exporter-title">Data Exporter for Helium</div>
     <div id="helium-exporter-help">
-      <a href="https://knowpulse.usask.ca/help" target="_blank" alt="help" title="help"><img src="<?php print $path_module . '/theme/images/help.gif'; ?>" style="border: none;" height="25" width="25"></a>
+      <?php if ($help_url != '#') { ?>
+        <a href="<?php print $help_url; ?>" target="_blank" alt="help" title="help"><img src="<?php print $path_module . '/theme/images/help.gif'; ?>" style="border: none;" height="25" width="25"></a>
+      <?php } ?>
+      &nbsp;
     </div>
     <div class="helium-exporter-clear-float"></div>
   </div>
@@ -37,8 +40,10 @@
             <img src="<?php print $path_module . '/theme/images/helium-export-step-02.gif'; ?>" style="border: none" alt="Step 02 : Export Data" title="Step 02 : Export Data" />
             <strong>Step #2:</strong>
             <p>
-              Use the built-in data exporter below to generate a data file. Click the link below for guide on using the exporter.
-              <a href="#">&#xbb; How to download data</a>
+              Use the built-in data exporter below to generate a data file.<?php if ($help_url != '#') { ?> Click the link below for guide on using the exporter.
+              <a href="<?php print $help_url; ?>">&#xbb; How to download data</a>
+              <?php } ?>
+              &nbsp;
             </p>
           </div>
           
@@ -46,8 +51,10 @@
             <img src="<?php print $path_module . '/theme/images/helium-export-step-03.gif'; ?>" style="border: none" alt="Step 03 : Load Data" title="Step 03 : Load Data" />
             <strong>Step #3:</strong>
             <p>
-              Load the data file generated in step #2 into Helium to visualize. Click the link below to explore and how to use Helium.
-              <a href="#">&#xbb; How to load data into Helium</a>
+              Load the data file generated in step #2 into Helium to visualize.<?php if ($help_url != '#') { ?> Click the link below to explore and how to use Helium.
+              <a href="<?php print $help_url; ?>">&#xbb; How to load data into Helium</a>
+              <?php } ?>
+              &nbsp;
             </p>
           </div>
           <div class="helium-exporter-clear-float"></div>                
